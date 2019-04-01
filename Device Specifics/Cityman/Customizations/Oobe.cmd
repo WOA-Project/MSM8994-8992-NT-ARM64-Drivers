6f3d841f235765a0adc7f8f8b276e609feca1473
Gus <gus33000@live.fr>
2019-04-01 11:57:52 +0200

    Update Oobe.cmd

--- Device Specifics/Cityman/Customizations/Oobe.cmd
+++ Device Specifics/Cityman/Customizations/Oobe.cmd
@@ -1,3 +1,6 @@
 @echo off
-\Windows\OEM\devcon.exe install \Windows\OEM\oempanel.inf ACPI\MSHW1004
+if exist \Windows\OEM\oobe.marker goto :eof
 \Windows\OEM\devcon.exe update \Windows\OEM\ChargeArbitration.inf Root\CAD
+\Windows\OEM\devcon.exe install \Windows\OEM\oempanel.inf ACPI\MSHW1004
+echo. > \Windows\OEM\oobe.marker
+shutdown /r /t 0 /f
