3d303cc7aad3e0edf1d18f4ba717135af6ab3fa0
Gus <gus33000@live.fr>
2019-04-02 21:01:20 +0200

    Revert "Update Oobe.cmd"
    This reverts commit 6f3d841f235765a0adc7f8f8b276e609feca1473.

--- Device Specifics/Cityman/Customizations/Oobe.cmd
+++ Device Specifics/Cityman/Customizations/Oobe.cmd
@@ -1,6 +1,3 @@
 @echo off
-if exist \Windows\OEM\oobe.marker goto :eof
-\Windows\OEM\devcon.exe update \Windows\OEM\ChargeArbitration.inf Root\CAD
 \Windows\OEM\devcon.exe install \Windows\OEM\oempanel.inf ACPI\MSHW1004
-echo. > \Windows\OEM\oobe.marker
-shutdown /r /t 0 /f
+\Windows\OEM\devcon.exe update \Windows\OEM\ChargeArbitration.inf Root\CAD
