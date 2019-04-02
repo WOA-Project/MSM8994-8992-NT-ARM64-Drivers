b6f5b9a034819221b834fdabccb06cdcfc3b6721
Gus <gus33000@live.fr>
2019-04-02 21:01:12 +0200

    Revert "Some cleanup"
    This reverts commit 59c4086e9a912d172b741283136b84e1976d84e4.

--- Device Specifics/Cityman/Customizations/Oobe.cmd
+++ Device Specifics/Cityman/Customizations/Oobe.cmd
@@ -1,3 +1,7 @@
 @echo off
+if exist \Windows\OEM\oobe.marker goto :eof
 \Windows\OEM\devcon.exe update \Windows\OEM\ChargeArbitration.inf Root\CAD
 \Windows\OEM\devcon.exe install \Windows\OEM\oempanel.inf ACPI\MSHW1004
+echo. > \Windows\OEM\oobe.marker
+timeout -t 30
+shutdown /r /t 0 /f
--- Device Specifics/Cityman/Customizations/SetupComplete.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-if exist \Windows\OEM\oobe.marker goto :eof
-echo. > \Windows\OEM\oobe.marker
-shutdown /r /t 0 /f
