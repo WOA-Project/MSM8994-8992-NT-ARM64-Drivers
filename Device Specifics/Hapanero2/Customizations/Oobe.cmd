983d9cb5f79f8ad86d4e710707309236d165d849
Gus <gus33000@live.fr>
2019-04-02 21:28:56 +0200

    More updates to post installation

--- Device Specifics/Hapanero2/Customizations/Oobe.cmd
+++ Device Specifics/Hapanero2/Customizations/Oobe.cmd
@@ -1,3 +1,2 @@
 @echo off
-\Windows\OEM\devcon.exe install \Windows\OEM\oempanel.inf ACPI\MSHW1004
-\Windows\OEM\devcon.exe update \Windows\OEM\ChargeArbitration.inf Root\CAD
+powershell.exe -command "Import-StartLayout -LayoutPath \\Windows\\OEM\\TaskbarLayoutModification.xml -MountPath $env:SystemDrive\\"
