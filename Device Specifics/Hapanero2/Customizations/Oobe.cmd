6c2a317ab682fdcc7a15c062f71f639f4b61bf21
Gus <gus33000@live.fr>
2019-04-02 21:49:46 +0200

    Updates to NFC installation procedure

--- Device Specifics/Hapanero2/Customizations/Oobe.cmd
+++ Device Specifics/Hapanero2/Customizations/Oobe.cmd
@@ -1,2 +1,4 @@
 @echo off
+for /f "delims=*" %%f in ('dir /b \Windows\System32\DriverStore\FileRepository\msnfc*.inf') do \Windows\OEM\devcon.exe update %%f ACPI\PN547
+for /f "delims=*" %%f in ('dir /b \Windows\System32\DriverStore\FileRepository\NXPP*.inf') do \Windows\OEM\devcon.exe update %%f ACPI\PN547
 powershell.exe -command "Import-StartLayout -LayoutPath \\Windows\\OEM\\TaskbarLayoutModification.xml -MountPath $env:SystemDrive\\"
--- Device Specifics/Talkman/Customizations/Oobe.cmd
+++ Device Specifics/Talkman/Customizations/Oobe.cmd
@@ -1,3 +1,5 @@
 @echo off
 \Windows\OEM\devcon.exe update \Windows\OEM\ChargeArbitration.inf Root\CAD
+for /f "delims=*" %%f in ('dir /b \Windows\System32\DriverStore\FileRepository\msnfc*.inf') do \Windows\OEM\devcon.exe update %%f ACPI\PN547
+for /f "delims=*" %%f in ('dir /b \Windows\System32\DriverStore\FileRepository\NXPP*.inf') do \Windows\OEM\devcon.exe update %%f ACPI\PN547
 powershell.exe -command "Import-StartLayout -LayoutPath \\Windows\\OEM\\TaskbarLayoutModification.xml -MountPath $env:SystemDrive\\"
