df7b72a33a29aa52f790131d7a8496ce38c004d6
Gus <gus33000@live.fr>
2019-04-03 15:32:55 +0200

    Final touches

--- Device Specifics/Cityman/Customizations/Oobe.cmd
+++ Device Specifics/Cityman/Customizations/Oobe.cmd
@@ -2,4 +2,4 @@
 \Windows\OEM\devcon.exe update \Windows\OEM\ChargeArbitration.inf Root\CAD
 for /f "delims=*" %%f in ('dir /b /s \Windows\System32\DriverStore\FileRepository\msnfc*.inf') do \Windows\OEM\devcon.exe update %%f ACPI\PN547
 for /f "delims=*" %%f in ('dir /b /s \Windows\System32\DriverStore\FileRepository\NXPP*.inf') do \Windows\OEM\devcon.exe update %%f ACPI\PN547
-powershell.exe -command "Import-StartLayout -LayoutPath \\Windows\\OEM\\TaskbarLayoutModification.xml -MountPath $env:SystemDrive\\"
+powershell.exe -command "Import-StartLayout -LayoutPath '\Windows\OEM\TaskbarLayoutModification.xml' -MountPath $env:SystemDrive"
