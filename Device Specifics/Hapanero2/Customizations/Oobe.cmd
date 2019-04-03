cc36c8d9fcb97660697de6f8e53c0319c18b810e
Gus <gus33000@live.fr>
2019-04-03 19:49:51 +0200

    Revert "Fix attempt for pre oobe provisioning"
    This reverts commit b6ec00628929e7b7ebb5cb3f708bba421cfcd9b0.

--- Device Specifics/Hapanero2/Customizations/Oobe.cmd
+++ Device Specifics/Hapanero2/Customizations/Oobe.cmd
@@ -1,2 +1,5 @@
 @echo off
-\Windows\OEM\OobeProvisioner.exe
+for /f "delims=*" %%f in ('dir /b /s \Windows\System32\DriverStore\FileRepository\msnfc*.inf') do \Windows\OEM\devcon.exe update %%f ACPI\PN547
+for /f "delims=*" %%f in ('dir /b /s \Windows\System32\DriverStore\FileRepository\NXPP*.inf') do \Windows\OEM\devcon.exe update %%f ACPI\PN547
+for /f "delims=*" %%f in ('dir /b /s \Windows\System32\DriverStore\FileRepository\qcxhcifilter*.inf') do \Windows\OEM\devcon.exe update %%f ACPI\QCOM24B6&HOST
+powershell.exe -command "Import-StartLayout -LayoutPath '\Windows\OEM\TaskbarLayoutModification.xml' -MountPath $env:SystemDrive"
--- Device Specifics/Talkman/Customizations/Oobe.cmd
+++ Device Specifics/Talkman/Customizations/Oobe.cmd
@@ -1,2 +1,5 @@
 @echo off
-\Windows\OEM\OobeProvisioner.exe
+\Windows\OEM\devcon.exe update \Windows\OEM\ChargeArbitration.inf Root\CAD
+for /f "delims=*" %%f in ('dir /b /s \Windows\System32\DriverStore\FileRepository\msnfc*.inf') do \Windows\OEM\devcon.exe update %%f ACPI\PN547
+for /f "delims=*" %%f in ('dir /b /s \Windows\System32\DriverStore\FileRepository\NXPP*.inf') do \Windows\OEM\devcon.exe update %%f ACPI\PN547
+powershell.exe -command "Import-StartLayout -LayoutPath '\Windows\OEM\TaskbarLayoutModification.xml' -MountPath $env:SystemDrive"
