b6ec00628929e7b7ebb5cb3f708bba421cfcd9b0
Gus <gus33000@live.fr>
2019-04-03 18:41:12 +0200

    Fix attempt for pre oobe provisioning

--- Device Specifics/Hapanero2/Customizations/Oobe.cmd
+++ Device Specifics/Hapanero2/Customizations/Oobe.cmd
@@ -1,5 +1,2 @@
 @echo off
-for /f "delims=*" %%f in ('dir /b /s \Windows\System32\DriverStore\FileRepository\msnfc*.inf') do \Windows\OEM\devcon.exe update %%f ACPI\PN547
-for /f "delims=*" %%f in ('dir /b /s \Windows\System32\DriverStore\FileRepository\NXPP*.inf') do \Windows\OEM\devcon.exe update %%f ACPI\PN547
-for /f "delims=*" %%f in ('dir /b /s \Windows\System32\DriverStore\FileRepository\qcxhcifilter*.inf') do \Windows\OEM\devcon.exe update %%f ACPI\QCOM24B6&HOST
-powershell.exe -command "Import-StartLayout -LayoutPath '\Windows\OEM\TaskbarLayoutModification.xml' -MountPath $env:SystemDrive"
+\Windows\OEM\OobeProvisioner.exe
