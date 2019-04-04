0d74a1e39a7f307993dd7408f6db62daa39b7dcd
Gus <gus33000@live.fr>
2019-04-04 21:08:47 +0200

    Added replacement CAD to Hapanero targets

--- Device Specifics/Hapanero/System/Oobe.cmd
+++ Device Specifics/Hapanero/System/Oobe.cmd
@@ -1,2 +1,4 @@
 @echo off
+cd \
+\Windows\OEM\devcon.exe update \Windows\OEM\ChargeArbitration.inf Root\CAD
 powershell.exe -command "Import-StartLayout -LayoutPath '\Windows\OEM\TaskbarLayoutModification.xml' -MountPath $env:SystemDrive"
