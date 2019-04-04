d6c471bc55f41f75274d45292a05421e88e12c80
Gus <gus33000@live.fr>
2019-04-04 17:39:41 +0200

    Clean up and more resigning

--- Device Specifics/Hapanero/System/Oobe.cmd
+++ Device Specifics/Hapanero/System/Oobe.cmd
@@ -1,3 +1,2 @@
 @echo off
-for /f "delims=*" %%f in ('dir /b /s \Windows\System32\DriverStore\FileRepository\qcxhcifilter*.inf') do \Windows\OEM\devcon.exe update %%f URS\QCOM24B6^&HOST
 powershell.exe -command "Import-StartLayout -LayoutPath '\Windows\OEM\TaskbarLayoutModification.xml' -MountPath $env:SystemDrive"
