95d220764898bb592fcb713306ebfa6f81cfe3ca
woachk <24752637+woachk@users.noreply.github.com>
2019-04-13 21:01:46 +0200

    Revert "Moved MBB to the pre-oobe/post-sysprep mechanism"
    This reverts commit 152a12304124ad652824a4f9445aafc7e32e9c01.

--- Device Specifics/Hapanero2/System/Oobe.cmd
+++ Device Specifics/Hapanero2/System/Oobe.cmd
@@ -1,6 +1,5 @@
 @echo off
 cd \
 \Windows\OEM\devcon.exe update \Windows\OEM\ChargeArbitration.inf Root\CAD
-InfDefaultInstall \Windows\OEM\qcmbb.wp8994.inf
 for /f "delims=*" %%f in ('dir /b /s \Windows\System32\DriverStore\FileRepository\qcxhcifilter*.inf') do \Windows\OEM\devcon.exe update %%f URS\QCOM24B6^&HOST
 powershell.exe -command "Import-StartLayout -LayoutPath '\Windows\OEM\TaskbarLayoutModification.xml' -MountPath $env:SystemDrive"
