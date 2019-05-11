576aa490c9cdc592c87770df941cabeb9d338ca6
Gus <gus33000@live.fr>
2019-05-11 19:22:56 +0200

    Alternative method for GPU driver installation
    Now that the touch driver is more reliable, we can afford to do this

--- Device Specifics/MMO/System/Oobe.cmd
+++ Device Specifics/MMO/System/Oobe.cmd
@@ -1,4 +1,5 @@
 @echo off
 cd \
+\Windows\OEM\devcon.exe update \Windows\OEM\oempanel.inf ACPI\MSHW1004
 \Windows\OEM\devcon.exe update \Windows\OEM\ChargeArbitration.inf Root\CAD
 for /f "delims=*" %%f in ('dir /b /s \Windows\System32\DriverStore\FileRepository\qcxhcifilter*.inf') do \Windows\OEM\devcon.exe update %%f URS\QCOM24B6^&HOST
