3159d1c37554525b4bb41dccceac68849135da68
Gus <gus33000@live.fr>
2019-05-13 19:06:01 +0200

    Fixed a GPU installation issue

--- Device Specifics/MMO/Customizations/Oobe.cmd
+++ Device Specifics/MMO/Customizations/Oobe.cmd
@@ -1,6 +1,5 @@
 @echo off
 cd \
-\Windows\OEM\devcon.exe update \Windows\OEM\oempanel.inf ACPI\MSHW1004
 \Windows\OEM\devcon.exe update \Windows\OEM\qcmbb.wp8994.inf QCMS\QCOM0EA0
 for /f "delims=*" %%f in ('dir /b /s \Windows\System32\DriverStore\FileRepository\qcxhcifilter*.inf') do \Windows\OEM\devcon.exe update %%f URS\QCOM24B6^&HOST
 \Windows\OEM\devcon.exe update \Windows\OEM\ChargeArbitration.inf Root\CAD
