f4af19edc5697010cc807a29af5c5fd307c4bd12
Gus <gus33000@live.fr>
2019-06-09 15:15:00 +0200

    Fixed Windows 10 20H1 issues with NFC

--- configurations/specifics-mmo/Customizations/SpecializeCommands.cmd
+++ configurations/specifics-mmo/Customizations/SpecializeCommands.cmd
@@ -20,10 +20,20 @@ powercfg /setacvalueindex OVERLAY_SCHEME_MAX SUB_PROCESSOR PERFEPP 25
 REM Disable fast startup as this is not supported by firmware and will cause issues
 REG ADD "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Power" /v HiberbootEnabled /t REG_DWORD /d 0 /f
 
+REM NFC
+powershell -command "[System.Environment]::OSVersion.Version.Build" > psout.txt
+set /p osbuild=<psout.txt
+del psout.txt
+
+set ossupport=1803
+if %osbuild% gtr 17134 set ossupport=1809
+if %osbuild% gtr 17763 set ossupport=1903
+
+copy \Windows\OEM\MSNfcI2C547.%ossupport%.dll \Windows\OEM\MSNfcI2C547.dll
+copy \Windows\OEM\nxppn547.%ossupport%.cat \Windows\OEM\nxppn547.cat
+
 REM System apps
 dism.exe /Online /Add-ProvisioningPackage /PackagePath:%SystemDrive%\Windows\Provisioning\Packages\OEMApps.ppkg
 
 cd \
 \Windows\OEM\devcon.exe update \Windows\OEM\oempanel.inf ACPI\MSHW1004
-
-call \Windows\OEM\InstallNfc.cmd
