1fcab5e8c5f112d4a9d3b2e09465c51fe2950aa7
woachk <24752637+woachk@users.noreply.github.com>
2019-06-10 22:38:00 +0200

    Revert "Very temporary workaround for 8992 devices until we get 8992 pep"
    This reverts commit d43196dd5e842fbb98afdf8f985273df06bdc85e.

--- configurations/specifics-mmo/Customizations/Oobe.cmd
+++ configurations/specifics-mmo/Customizations/Oobe.cmd
@@ -3,4 +3,4 @@ cd \
 \Windows\OEM\devcon.exe update \Windows\OEM\qcmbb.wp8994.inf QCMS\QCOM0EA0
 for /f "delims=*" %%f in ('dir /b /s \Windows\System32\DriverStore\FileRepository\qcxhcifilter*.inf') do \Windows\OEM\devcon.exe update %%f URS\QCOM24B6^&HOST
 \Windows\OEM\devcon.exe update \Windows\OEM\ChargeArbitration.inf Root\CAD
-if NOT "%NUMBER_OF_PROCESSORS%" == "6" \Windows\OEM\devcon.exe update \Windows\OEM\NXPPN547.inf ACPI\PN547
+\Windows\OEM\devcon.exe update \Windows\OEM\NXPPN547.inf ACPI\PN547
